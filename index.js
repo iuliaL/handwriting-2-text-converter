e178e393925dc0f96b033bffa4aaeedbf46b4e99
iulial <iuliamarialungu@yahoo.com>
2023-01-16 16:14:02 +0200

    updated to include language hint

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-  // Import the Google Cloud client library
-  const vision = require("@google-cloud/vision");
+// Import the Google Cloud client library
+const vision = require("@google-cloud/vision");
 const fs = require("fs");
-
 
 async function detectFulltext(fileName, lang) {
   // Create a client with service account credentials to download from the google cloud console
   const client = new vision.ImageAnnotatorClient({
     keyFilename: "./private-credentials-diary-374313-20541ad5e3b8.json",
   });
-  
+  const content = fs.readFileSync(fileName);
   const EN = "en-t-i0-handwrit";
   const RO = "ro-t-i0-handwrit";
   const prefferedLanguage = lang === EN ? [EN] : lang === RO ? [RO] : [RO, EN];
 
   const request = {
     image: {
-      content: fs.readFileSync(fileName),
+      content,
     },
     imageContext: {
       languageHints: prefferedLanguage,
