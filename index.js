9a01b2adbb452cc9fdbe5960ae1fe1f90ef6dbaa
iulial <iuliamarialungu@yahoo.com>
2023-01-16 15:48:15 +0200

    updated to include language hint

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-async function detectFulltext(fileName) {
-  // Imports the Google Cloud client library
-  const vision = require('@google-cloud/vision');
+  // Import the Google Cloud client library
+  const vision = require("@google-cloud/vision");
+const fs = require("fs");
 
-  // Creates a client with service account credentials to download from the google cloud console
+
+async function detectFulltext(fileName, lang) {
+  // Create a client with service account credentials to download from the google cloud console
   const client = new vision.ImageAnnotatorClient({
-    keyFilename: "./private-credentials-diary-374313-20541ad5e3b8.json"
+    keyFilename: "./private-credentials-diary-374313-20541ad5e3b8.json",
   });
+  
+  const EN = "en-t-i0-handwrit";
+  const RO = "ro-t-i0-handwrit";
+  const prefferedLanguage = lang === EN ? [EN] : lang === RO ? [RO] : [RO, EN];
 
-  // Read a local image as a text document
-  const [result] = await client.documentTextDetection(fileName);
+  const request = {
+    image: {
+      content: fs.readFileSync(fileName),
+    },
+    imageContext: {
+      languageHints: prefferedLanguage,
+    },
+  };
+
+  // Read the local image as a text document
+  const [result] = await client.documentTextDetection(request);
   const fullTextAnnotation = result.fullTextAnnotation;
-  console.log(`TEXT INIT:\n${fullTextAnnotation.text}\n`);
-  fullTextAnnotation.pages.forEach((page, index) => {
-    console.log("Page", index + 1, page);
-  });
+  console.log(`INIT -------- \n${fullTextAnnotation.text}\n`);
+  // fullTextAnnotation.pages.forEach((page, index) => {
+  //   console.log("Page", index + 1, page);
+  // });
 }
 
-detectFulltext("./laura.png")
+detectFulltext("./images/07_01_2021/IMG_4584.png", "RO");
