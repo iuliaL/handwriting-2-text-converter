bacd1175858521ce25f97df4abb89a7009313885
iulial <iuliamarialungu@yahoo.com>
2023-02-10 17:09:46 +0100

    Change rename file helper to go recursively

--- file-helpers/regex_rename.py
+++ file-helpers/regex_rename.py
@@ -1,31 +1,36 @@
-#  Bulk rename files from 23_03_2012.txt to 2012-03-23.md (Desired name for Obsidian)
-# change path with the desired directory path
-
+#  Bulk rename files from 23_03_2012.txt to 2012-03-23.md (Desired name for Obsidian) 
+# Goes recursively for directories.
+# Change path with the desired directory path
 import os
 import re
 
+
+def rename_files(path):
+    pattern_to_capture = r'(\d{2})_(\d{2})_(\d{4}).txt'
+    replace_with = r"\3-\2-\1.md"
+
+    comp = re.compile(pattern_to_capture)
+    for f in os.listdir(path):
+        full_path = os.path.join(path, f)
+        if os.path.isfile(full_path):
+
+            match = comp.search(f)
+            if not match:
+                continue
+
+            try:
+                new_name = match.expand(replace_with)
+                new_name = os.path.join(path, new_name)
+            except re.error:
+                continue
+
+            if os.path.isfile(new_name):
+                print('%s -> %s skipped' % (f, new_name))
+            else:
+                os.rename(full_path, new_name)
+        elif os.path.isdir(full_path):
+            rename_files(full_path)
+
+
 home_path = os.path.expanduser('~')
-path = home_path + '/Desktop/diary-entries/to-rename'
-
-pattern_to_capture = r'(\d{2})_(\d{2})_(\d{4}).*$'
-replace_with = r"\3-\2-\1.md"
-
-comp = re.compile(pattern_to_capture)
-for f in os.listdir(path):
-    full_path = os.path.join(path, f)
-    if os.path.isfile(full_path):
-
-        match = comp.search(f)
-        if not match:
-            continue
-
-        try:
-            new_name = match.expand(replace_with)
-            new_name = os.path.join(path, new_name)
-        except re.error:
-            continue
-
-        if os.path.isfile(new_name):
-            print('%s -> %s skipped' % (f, new_name))
-        else:
-            os.rename(full_path, new_name)
+rename_files(home_path + '/Desktop/diary-entries/handwritten')
